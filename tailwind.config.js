8b1c22a0640c69496fed2b78e1df881fd0c686f5
Md. Minhazur Rahman <107420699+robot-76@users.noreply.github.com>
2025-07-13 00:11:28 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#111827',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
